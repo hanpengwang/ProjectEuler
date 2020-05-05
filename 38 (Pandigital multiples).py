15d2c50c813feb9c18098893709f84cae6469118
HanPeng Wang <hanpengwang@HanPengs-MacBook-Air.local>
2020-05-05 18:53:53 -0300

    update

--- 38 (Pandigital multiples).py
+++ 38 (Pandigital multiples).py
@@ -7,10 +7,23 @@ Created on Sat Jan 18 15:40:48 2020
 """
 
 def Pandigital_multiples():
-    upper = 10 ** 9 
-    lower = 10 ** 8 + 1
-    for i in range(upper)[lower:]:
-        i+=2
+    MAX = 0
+    number = 1 
+    while True:
+        pan = ""
+        for n in range(1,10):
+            pan = pan + str(number*n)
+            if len(pan)>=9:
+                break
+        if len(pan) == 9 and sorted(list(pan)) == ["1","2","3","4","5","6","7","8","9"]:
+            MAX = max(MAX, int(pan))
+            
+        if len(pan)>9 and n == 2:
+            break
         
+        number += 1
+    return MAX
+            
+            
 
-Pandigital_multiples()
+print(Pandigital_multiples())
--- 50 (Consecutive prime sum).py
+++ 50 (Consecutive prime sum).py
@@ -20,10 +20,39 @@ def primeFactor(n):
     return True if len(primes) == 0 else False
 
 
-def Cons_prime():
-    num = 0
-    prime_test = 2
-    while num <
+def Prime_gen():
+    prime_l = []
+    for n in range(2,10**4+1):
+        if primeFactor(n):
+            prime_l.append(n)
+   
+    return prime_l
+
+
+
+def Con_prime_sum():
+    prime_l = Prime_gen()
+    count_len = 0 
+    prime = 0
     
+    for i in range(1,len(prime_l)+1):
+        partial_l = prime_l[i:]
+        len_partial = len(partial_l)
+        if len_partial <= count_len:
+            break
+        
+        for i2 in range(1, len_partial)[::-1]:
+            subset = partial_l[1:i2+1]
+            _sum = sum(subset)
+            if _sum < 10**6 and primeFactor(_sum):
+                if count_len < len(subset):
+                    count_len = len(subset)
+                    prime = _sum
+                break
+    
+    return prime
+    
+
+print(Con_prime_sum())
 
 
